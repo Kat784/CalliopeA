d7a199cafc6dd76ceb44376ce1fa5ed2ef4eaa02
Kat784 <134415256+Kat784@users.noreply.github.com>
2023-06-14 09:19:27 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ enum RadioMessage {
     message1 = 49434
 }
 input.onButtonEvent(Button.AB, ButtonEvent.LongClick, function () {
+    radio.setGroup(1)
     basic.showLeds(`
         # # . . .
         # # . . .
