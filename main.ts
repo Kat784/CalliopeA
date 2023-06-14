5f6b4fea4f3c84a85b7f9ae2ba4f35af75acc1c6
Kat784 <134415256+Kat784@users.noreply.github.com>
2023-06-14 10:49:28 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,28 @@
 enum RadioMessage {
-    message1 = 49434
+    message1 = 49434,
+    Aufgabe = 15415
 }
-input.onButtonEvent(Button.AB, ButtonEvent.LongClick, function () {
-    radio.setGroup(1)
-    basic.showLeds(`
-        # # . . .
-        # # . . .
-        . . . . .
-        . . . # #
-        . . . # #
-        `)
-    basic.pause(500)
-    basic.clearScreen()
-    radio.sendNumber(faktor1)
-    radio.sendString("*")
-    radio.sendNumber(faktor2)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == Ergebnis) {
+        radio.sendNumber(1)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        basic.setLedColor(0xffff00)
+    } else {
+        radio.sendNumber(2)
+    }
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.turnRgbLedOff()
+    basic.clearScreen()
     Antwort = 0
     Rechenaufgabe()
-    basic.pause(500)
+    basic.pause(200)
     basic.showNumber(Antwort)
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
@@ -32,6 +35,7 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
             . # # # .
             `)
         basic.setLedColor(0x00ff00)
+        radio.sendNumber(3)
     } else {
         basic.showLeds(`
             . . . . .
@@ -42,23 +46,21 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
             `)
         basic.setLedColor(0xff0000)
     }
-    basic.pause(2000)
-    basic.clearScreen()
-    basic.turnRgbLedOff()
 })
 function Rechenaufgabe () {
     faktor1 = randint(1, 9)
     faktor2 = randint(1, 9)
     Ergebnis = faktor1 * faktor2
-    basic.showNumber(faktor1)
-    basic.showString("*")
-    basic.showNumber(faktor2)
+    radio.sendString("" + faktor1 + "*" + faktor2)
+    basic.showString("" + faktor1 + "*" + faktor2)
     basic.clearScreen()
 }
-let Ergebnis = 0
-let Antwort = 0
 let faktor2 = 0
 let faktor1 = 0
+let Antwort = 0
+let Ergebnis = 0
+basic.showString("A")
+radio.setGroup(1)
 game.setScore(0)
 basic.forever(function () {
     if (input.isGesture(Gesture.TiltLeft)) {
