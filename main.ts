5d9ac530874236adf839a157dc90fcc4973c2ceb
Kat784 <134415256+Kat784@users.noreply.github.com>
2023-07-13 12:20:11 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,8 +13,18 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . . .
             `)
         basic.setLedColor(0xffff00)
+        PunkteB += 1
     } else {
         radio.sendNumber(2)
+    }
+    if (PunkteB == 2) {
+        radio.sendNumber(4)
+        music.playTone(233, music.beat(BeatFraction.Whole))
+        music.playTone(208, music.beat(BeatFraction.Whole))
+        music.playTone(185, music.beat(BeatFraction.Whole))
+        music.playTone(165, music.beat(BeatFraction.Whole))
+        PunkteA = 0
+        PunkteB = 0
     }
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
@@ -36,7 +46,7 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
             `)
         basic.setLedColor(0x00ff00)
         radio.sendNumber(3)
-        game.addScore(1)
+        PunkteA += 1
     } else {
         basic.showLeds(`
             . . . . .
@@ -47,8 +57,9 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
             `)
         basic.setLedColor(0xff0000)
     }
-    if (game.score() == 2) {
+    if (PunkteA == 2) {
         Sieg()
+        PunkteA = 0
     }
 })
 function Rechenaufgabe () {
@@ -136,12 +147,17 @@ let faktor2 = 0
 let faktor1 = 0
 let Antwort = 0
 let Ergebnis = 0
+let PunkteB = 0
+let PunkteA = 0
 basic.showString("A")
 radio.setGroup(1)
-game.setScore(0)
 let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
-_4digit.bit(0, 1)
+PunkteA = 0
+PunkteB = 0
 basic.forever(function () {
+    _4digit.bit(PunkteA, 0)
+    _4digit.point(true)
+    _4digit.bit(PunkteB, 3)
     if (input.isGesture(Gesture.TiltLeft)) {
         Antwort += 1
         basic.showNumber(Antwort)
