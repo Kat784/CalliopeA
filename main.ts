a3dd5ea4030408f5d5a50c5cfdf290f5e496fbfc
Kat784 <134415256+Kat784@users.noreply.github.com>
2023-07-13 18:13:07 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ enum RadioMessage {
 }
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == Ergebnis) {
-        Pause = true
+        Knopf_B_an = false
         radio.sendNumber(1)
         basic.showLeds(`
             . . . . .
@@ -19,7 +19,7 @@ radio.onReceivedNumber(function (receivedNumber) {
         radio.sendNumber(2)
     }
     if (PunkteB == 2) {
-        Pause = true
+        Knopf_B_an = false
         radio.sendNumber(20)
         basic.showLeds(`
             . . . . .
@@ -29,59 +29,64 @@ radio.onReceivedNumber(function (receivedNumber) {
             # . . . #
             `)
         basic.setLedColor(0xff0000)
-        basic.pause(3500)
+        basic.pause(4000)
         music.playTone(233, music.beat(BeatFraction.Whole))
         music.playTone(208, music.beat(BeatFraction.Whole))
         music.playTone(185, music.beat(BeatFraction.Whole))
         music.playTone(165, music.beat(BeatFraction.Whole))
         PunkteA = 0
         PunkteB = 0
+        basic.clearScreen()
+        basic.turnRgbLedOff()
     }
-    Neue_Aufgabe = true
+    Knopf_A_an = true
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    if (Neue_Aufgabe) {
+    if (Knopf_A_an) {
         basic.turnRgbLedOff()
         basic.clearScreen()
         Antwort = 0
         Rechenaufgabe()
         basic.pause(200)
         basic.showNumber(Antwort)
-        Neue_Aufgabe = false
-        Pause = false
+        Knopf_A_an = false
+        Knopf_B_an = true
     } else {
+        basic.pause(200)
         basic.showString("" + faktor1 + "*" + faktor2)
         basic.showNumber(Antwort)
     }
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    Pause = true
-    if (Antwort == Ergebnis) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-        basic.setLedColor(0x00ff00)
-        radio.sendNumber(3)
-        PunkteA += 1
-        Neue_Aufgabe = true
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . # # # .
-            # . . . #
-            `)
-        basic.setLedColor(0xff0000)
-    }
-    if (PunkteA == 2) {
-        radio.sendNumber(10)
-        Sieg()
-        PunkteA = 0
+    if (Knopf_B_an == true) {
+        Knopf_B_an = false
+        if (Antwort == Ergebnis) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+            basic.setLedColor(0x00ff00)
+            radio.sendNumber(3)
+            PunkteA += 1
+            Knopf_A_an = true
+        } else {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
+            basic.setLedColor(0xff0000)
+        }
+        if (PunkteA == 2) {
+            radio.sendNumber(10)
+            Sieg()
+            PunkteA = 0
+        }
     }
 })
 function Rechenaufgabe () {
@@ -164,13 +169,14 @@ function Sieg () {
         . . . . .
         `)
     basic.clearScreen()
+    basic.turnRgbLedOff()
 }
 let faktor2 = 0
 let faktor1 = 0
 let Antwort = 0
 let Ergebnis = 0
-let Pause = false
-let Neue_Aufgabe = false
+let Knopf_B_an = false
+let Knopf_A_an = false
 let PunkteB = 0
 let PunkteA = 0
 basic.showString("A")
@@ -178,13 +184,13 @@ radio.setGroup(1)
 let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
 PunkteA = 0
 PunkteB = 0
-Neue_Aufgabe = true
-Pause = false
+Knopf_A_an = true
+Knopf_B_an = true
 basic.forever(function () {
     _4digit.bit(PunkteA, 0)
     _4digit.point(true)
     _4digit.bit(PunkteB, 3)
-    if (Pause == false) {
+    if (Knopf_B_an == true) {
         if (input.isGesture(Gesture.TiltLeft)) {
             Antwort += 1
             basic.showNumber(Antwort)
